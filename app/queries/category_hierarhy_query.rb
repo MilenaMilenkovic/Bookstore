99ee21a706e2d7f9da34870302548553ae3f6e38
MilenaMilenkovic <milena.damjanovic@yandex.com>
2021-08-23 00:30:41 +0200

    MySQL5 -> MySQL8. Ussage of recursive CTE for getting the parent hierarhy.

--- app/queries/category_hierarhy_query.rb
+++ app/queries/category_hierarhy_query.rb
@@ -6,28 +6,47 @@ class CategoryHierarhyQuery < Query
   end
 
   def call
-    @relation.or(subcategories)
+    @relation.or(Category.where(id: subcategories.to_a.flatten))
   end
 
   private
 
     def subcategories
-      Category.where("id IN (#{hierarhy_sql})")
-    end
-
-    # As the app should support SQL version 5.7+, the query is defined by using variables
-    # More efficient way would be to use recursive CTE (supported by MySql 8+)
-    def hierarhy_sql
-      @relation.pluck(:id).map {|id| children_hierarhy(id) }.join(") UNION (")
-    end
-
-    def children_hierarhy(child_id)
-      <<~SQL
-          select categories.id
-          from categories,
-               (select @parent := #{child_id.to_i}) vars
-          where find_in_set(categories.parent_id, @parent) > 0 and @parent := concat(@parent, ',', categories.id)
+      Category.connection.execute <<~SQL
+        #{parents_cte}
+        SELECT * from #{parent_hierarhy_ctes.keys.join(' JOIN ')};
       SQL
+    end
+
+    def parents_cte
+      <<~SQL
+          WITH RECURSIVE
+          #{parent_hierarhy_ctes.values.join(', ')}
+      SQL
+    end
+
+    def parent_hierarhy_ctes
+      @parent_hierarhy_ctes ||= @relation.pluck(:id).inject({}) do |h, id|
+        h[cte_name(id)] = parent_hierarhy_cte(id)
+        h
+      end
+    end
+
+    def parent_hierarhy_cte(child_id)
+      cte = cte_name(child_id)
+      <<~SQL
+          #{cte} AS
+          (
+            SELECT id from categories WHERE id = #{child_id.to_i}
+            UNION ALL
+            SELECT c.id FROM categories c JOIN #{cte}
+            ON #{cte}.id=c.parent_id
+          )
+      SQL
+    end
+
+    def cte_name(id)
+      "parents#{id.to_i}"
     end
 
 end
